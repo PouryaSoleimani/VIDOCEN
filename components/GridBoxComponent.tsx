6875c637ec773f310b639fc806e815303402b80e
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-06 13:11:51 +0330

    VIDOCEN

--- components/GridBoxComponent.tsx
+++ components/GridBoxComponent.tsx
@@ -1,97 +1,22 @@
-import Image from 'next/image'
+
 import React from 'react'
 
 const GridBoxComponent = () => {
     return (
-        <div className="w-full py-4 rounded bg-zinc-950">
-            <div className="mx-auto px-2 ">
-                <div className="grid gap-4 grid-cols-1 lg:grid-cols-2">
 
-                    <div className="relative lg:row-span-2">
-                        <div className="absolute inset-px rounded-lg bg-white"></div>
+        <div className="grid grid-cols-3 md:grid-cols-3 grid-rows-4 md:grid-rows-4 gap-2 md:gap-2 m-4 text-center text-white font-bold text-xl md:text-3xl">
+            <div className="col-start-1 row-start-1 col-span-2 md:col-start-1 md:row-start-1 md:col-span-2 md:row-span-1 bg-black text-center font-bold rounded-md p-10">0</div>
+            <div className="GRID___1 col-start-3 row-start-1 md:col-start-3 md:row-start-1 md:col-span-1 md:row-span-1 bg-black font-bold rounded-md p-10 whitespace-nowrap flex items-center jus"><h2 className='mx-auto'>بی نهایت</h2> </div>
+            <div className="col-start-1 row-start-2 md:col-start-1 md:row-start-2 md:col-span-1 md:row-span-1 bg-black text-center font-bold rounded-md p-10">2</div>
+            <div className="col-start-2 row-start-2 md:col-start-2 md:row-start-2 md:col-span-1 md:row-span-1 bg-black text-center font-bold rounded-md p-10">3</div>
+            <div className="col-start-3 row-start-2 md:col-start-3 md:row-start-2 md:col-span-1 md:row-span-1 bg-black text-center font-bold rounded-md p-10">4</div>
+            <div className="col-start-1 row-start-3 md:col-start-1 md:row-start-3 md:col-span-1 md:row-span-1 bg-black text-center font-bold rounded-md p-10">5</div>
+            <div className="col-start-2 row-start-3 md:col-start-2 md:row-start-3 md:col-span-1 md:row-span-1 bg-black text-center font-bold rounded-md p-10">6</div>
+            <div className="col-start-3 row-start-3 md:col-start-3 md:row-start-3 md:col-span-1 md:row-span-1 bg-black text-center font-bold rounded-md p-10">7</div>
+            <div className="col-start-1 row-start-4 md:col-start-1 md:row-start-4 md:col-span-1 md:row-span-1 bg-black text-center font-bold rounded-md p-10">8</div>
+            <div className="col-start-2 row-start-4 md:col-start-2 md:row-start-4 md:col-span-1 md:row-span-1 bg-black text-center font-bold rounded-md p-10">9</div>
+            <div className="col-start-3 row-start-4 md:col-start-3 md:row-start-4 md:col-span-1 md:row-span-1 bg-black text-center font-bold rounded-md p-10">10</div>
 
-                        <div className="relative flex h-full flex-col overflow-hidden">
-                            <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
-                                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
-                                    Mobile friendly
-                                </p>
-                                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
-                                </p>
-                            </div>
-                            <div className="@container relative min-h-[30rem] w-full grow max-lg:mx-auto max-lg:max-w-sm">
-                                <div className="absolute inset-x-10 top-10 bottom-0 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl">
-                                    <img className="size-full object-cover object-top" src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-mobile-friendly.png" alt="" />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 lg:rounded-l-[2rem]"></div>
-                    </div>
-
-                    <div className="relative max-lg:row-start-1 GRID___1">
-                        <div className="absolute inset-px bg-black/10 rounded-lg"></div>
-                        <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
-                            <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Performance</p>
-
-                            </div>
-                            <div className="flex flex-1 items-center justify-center px-8 max-lg:pt-10 max-lg:pb-12 sm:px-10 lg:pb-2">
-                            </div>
-                        </div>
-                        <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-t-[2rem]"></div>
-                    </div>
-
-                    <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
-                        <div className="absolute inset-px rounded-lg bg-white"></div>
-                        <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)]">
-                            <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Security</p>
-                                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                                    Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi.
-                                </p>
-                            </div>
-                            <div className="@container flex flex-1 items-center max-lg:py-6 lg:pb-2">
-                                <img
-                                    className="h-[min(152px,40cqw)] object-cover"
-                                    src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-security.png"
-                                    alt=""
-                                />
-                            </div>
-                        </div>
-                        <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5"></div>
-                    </div>
-
-                    <div className="relative lg:row-span-2">
-                        <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
-                        <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
-                            <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
-                                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
-                                    Powerful APIs
-                                </p>
-                                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                                    Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget sem sodales gravida.
-                                </p>
-                            </div>
-                            <div className="relative min-h-[30rem] w-full grow">
-                                <div className="absolute top-10 right-0 bottom-0 left-10 overflow-hidden rounded-tl-xl bg-gray-900 shadow-2xl">
-                                    <div className="flex bg-gray-800/40 ring-1 ring-white/5">
-                                        <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
-                                            <div className="border-r border-b border-r-white/10 border-b-white/20 bg-white/5 px-4 py-2 text-white">
-                                                NotificationSetting.jsx
-                                            </div>
-                                            <div className="border-r border-gray-600/10 px-4 py-2">App.jsx</div>
-                                        </div>
-                                    </div>
-                                    <div className="px-6 pt-6 pb-14">{/* Your code example */}</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
-                    </div>
-
-
-                </div>
-            </div>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import HeaderTitleComponent from "@/components/HeaderTitleComponent";
 export default function Home() {
   return (
     <section
-      className={`bg-zinc-900 text-gray-100 antialiased flex flex-col min-h-screen`}>
+      className={`bg-zinc-900 text-gray-100 antialiased flex flex-col min-h-screen overflow-x-hidden`}>
       <NavbarComponent />
       <div className="px-4">
         <HeaderTitleComponent />
