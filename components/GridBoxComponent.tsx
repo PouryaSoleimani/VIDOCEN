3f3e684247dfa116ef01cd8e98c6767cbf59262e
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-06 12:40:03 +0330

    VIDOCEN

--- components/GridBoxComponent.tsx
+++ components/GridBoxComponent.tsx
@@ -2,17 +2,12 @@ import React from 'react'
 
 const GridBoxComponent = () => {
     return (
-        <div className="bg-gray-50 py-24 sm:py-32">
-            <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
-                <h2 className="text-center text-base/7 font-semibold text-indigo-600">Deploy faster</h2>
-
-                <p className="mx-auto mt-2 max-w-lg text-center text-4xl font-semibold tracking-tight text-balance text-gray-950 sm:text-5xl">
-                    Everything you need to deploy your app
-                </p>
-                <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
+        <div className="w-full py-4 rounded bg-zinc-950">
+            <div className="mx-auto px-2 ">
+                <div className="grid gap-4 grid-cols-1 lg:grid-cols-2">
                     <div className="relative lg:row-span-2">
-                        <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]"></div>
-                        <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
+                        <div className="absolute inset-px rounded-lg bg-white"></div>
+                        <div className="relative flex h-full flex-col overflow-hidden">
                             <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
                                 <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
                                     Mobile friendly
@@ -23,18 +18,14 @@ const GridBoxComponent = () => {
                             </div>
                             <div className="@container relative min-h-[30rem] w-full grow max-lg:mx-auto max-lg:max-w-sm">
                                 <div className="absolute inset-x-10 top-10 bottom-0 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl">
-                                    <img
-                                        className="size-full object-cover object-top"
-                                        src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-mobile-friendly.png"
-                                        alt=""
-                                    />
+                                    <img className="size-full object-cover object-top" src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-mobile-friendly.png" alt="" />
                                 </div>
                             </div>
                         </div>
                         <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 lg:rounded-l-[2rem]"></div>
                     </div>
                     <div className="relative max-lg:row-start-1">
-                        <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]"></div>
+                        <div className="absolute inset-px bg-black rounded-lg"></div>
                         <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
                             <div className="px-8 pt-8 sm:px-10 sm:pt-10">
                                 <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Performance</p>
--- components/NavbarComponent.tsx
+++ components/NavbarComponent.tsx
@@ -4,25 +4,27 @@ import React from 'react'
 
 const NavbarComponent = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light w-screen h-full flex items-center justify-between shadow-md px-4 py-5">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light w-screen h-full flex flex-row gap-y-4 items-center justify-between shadow-md px-4 py-5">
+
             <div id='LEFT' className='flex gap-x-2 pl-2 items-center'>
                 <MenuIcon />
                 <Image src="/Logo.svg" alt="Logo" width={150} height={250} className="navbar-brand" />
             </div>
-            <div id="CENTER" className='flex items-center justify-center w-1/3'>
+
+            <div id="CENTER" className='items-center justify-center hidden lg:flex w-1/3'>
                 <SearchIcon className='translate-x-10 w-8 h-5' />
                 <input type="search" name="search" id="NAVBAR___SEARCH" className='border-2 border-red-800 rounded-full px-4 py-1 w-full relative top-0 left-0' placeholder='جستجو کنید' dir='rtl' />
             </div>
 
             <div id="RIGHT">
                 <ul className="navbar-nav flex flex-row gap-x-4 pr-4 items-center">
-                    <li className="nav-item gap-x-1 flex items-center">
-                        <a className="nav-link bg-emerald-600 hover:bg-emerald-800 duration-300 text-white font-bold tracking-tight rounded px-4 py-1 text-xl" href="#">ورود</a>
-                        <a className="nav-link bg-sky-600 hover:bg-sky-800 duration-300 text-white font-bold tracking-tight rounded px-4 py-1 text-xl" href="#">ثبت نام</a>
+                    <li className="nav-item gap-x-1  items-center hidden lg:flex">
+                        <a className="text-sm nav-link bg-emerald-600 hover:bg-emerald-800 duration-300 text-white font-bold tracking-tight rounded px-4 py-1 lg:text-xl" href="#">ورود</a>
+                        <a className="text-sm nav-link bg-sky-600 hover:bg-sky-800 duration-300 text-white font-bold tracking-tight rounded px-4 py-1 whitespace-nowrap  lg:text-xl" href="#">ثبت نام</a>
                     </li>
                     <li className="nav-item flex gap-x-2">
-                        <Image src="/flag_iran.png" alt="Logo" width={50} height={250} className="navbar-brand rounded cursor-pointer" />
-                        <Image src="/flag_england.png" alt="Logo" width={50} height={250} className="navbar-brand rounded cursor-pointer" />
+                        <Image src="/flag_iran.png" alt="Logo" width={50} height={250} className="scale-75 lg:scale-100 navbar-brand rounded cursor-pointer" />
+                        <Image src="/flag_england.png" alt="Logo" width={50} height={250} className="scale-75 lg:scale-100 navbar-brand rounded cursor-pointer" />
                     </li>
                     <li>
                         <input type="checkbox" className="peer sr-only opacity-0" id="toggle" />
