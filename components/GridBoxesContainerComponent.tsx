f4baa99f413245022dc10f64b64c14c67e44d19c
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-06 15:11:56 +0330

    VIDOCEN

--- components/GridBoxesContainerComponent.tsx
+++ components/GridBoxesContainerComponent.tsx
@@ -5,8 +5,8 @@ const GridBoxesContainerComponent = () => {
     return (
 
         <div className="grid grid-cols-3 md:grid-cols-3 grid-rows-4 md:grid-rows-4 gap-2 md:gap-2 m-4 text-center text-white font-bold text-xl md:text-3xl h-screen">
-            <div className="GRID___1col-start-1 row-start-1 col-span-2 md:col-start-1 md:row-start-1 md:col-span-2 md:row-span-1 bg-red-950 text-center font-bold rounded-md p-10"><h2>بی نهایت</h2> </div>
-            <div className="col-start-3 row-start-1 md:col-start-3 md:row-start-1 md:col-span-1 md:row-span-1 bg-black font-bold rounded-md whitespace-nowrap flex items-center justify-center p-10"></div>
+            <div className="col-start-1 row-start-1 col-span-2 md:col-start-1 md:row-start-1 md:col-span-2 md:row-span-1 bg-red-950 text-center font-bold rounded-md p-10">0</div>
+            <div className="GRID___1 col-start-3 row-start-1 md:col-start-3 md:row-start-1 md:col-span-1 md:row-span-1 bg-black font-bold rounded-md whitespace-nowrap flex items-center justify-center p-10"><h2>بی نهایت</h2> </div>
             <div className="col-start-1 row-start-2 md:col-start-1 md:row-start-2 md:col-span-1 md:row-span-1  text-center font-bold rounded-md flex items-center justify-center p-10 bg-blue-950">2</div>
             <div className="col-start-2 row-start-2 md:col-start-2 md:row-start-2 md:col-span-1 md:row-span-1  text-center font-bold rounded-md flex items-center justify-center p-10 bg-green-950">3</div>
             <div className="col-start-3 row-start-2 md:col-start-3 md:row-start-2 md:col-span-1 md:row-span-1  text-center font-bold rounded-md flex items-center justify-center p-10 bg-yellow-950">4</div>
